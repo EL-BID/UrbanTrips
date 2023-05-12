4d8ec99c6eb4eef7cfbdf91a4081e6e8b2e08c7f
alephcero <4lephcero@gmail.com>
2023-05-12 12:17:12 +0200

    Add test for routes

--- urbantrips/tests/test_routes.py
+++ urbantrips/tests/test_routes.py
@@ -1,5 +1,17 @@
 from urbantrips.carto import routes
+from urbantrips.utils import utils
+import pandas as pd
 
 
 def test_routes():
     routes.process_routes_geoms()
+
+    conn_insumos = utils.iniciar_conexion_db(tipo='insumos')
+
+    lines_routes = pd.read_sql(
+        "select * from official_lines_geoms", conn_insumos)
+    branches_routes = pd.read_sql(
+        "select * from official_branches_geoms", conn_insumos)
+
+    assert len(lines_routes) == 2
+    assert len(branches_routes) == 8
