e70dc86acc0231ac4b4564b14dcf22d9f521852f
Felipe Gonzalez <felipegonz@gmail.com>
2023-06-19 16:53:39 +0200

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,14 +7,14 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'urbantrips'
-copyright = '2023, felipe & sebastian'
-author = 'felipe & sebastian'
+copyright = '2023, Felipe Gonzalez & Sebastian Anapolsky'
+author = 'Felipe Gonzalez & Sebastian Anapolsky'
 release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +24,5 @@ language = 'es'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
