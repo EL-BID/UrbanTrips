534859e39dc93f01f9afd4178d8c9e86a67cc850
alephcero <4lephcero@gmail.com>
2023-10-12 15:21:16 -0300

    update create viz

--- urbantrips/create_viz.py
+++ urbantrips/create_viz.py
@@ -4,6 +4,7 @@ from urbantrips.kpi import kpi
 from urbantrips.viz_ppt_utils import viz_ppt_utils
 from urbantrips.utils import utils
 from urbantrips.utils.check_configs import check_config
+from urbantrips.utils.utils import (leer_configs_generales)
 
 
 def main():
@@ -11,27 +12,40 @@ def main():
 
     conn_data = utils.iniciar_conexion_db(tipo='data')
 
-    print("Leyendo datos de las lineas con más demanda")
-    # get top 5 trx id_lines
-    q = """
-        select id_linea
-        from etapas e
-        where modo = 'autobus'
-        group by id_linea
-        order by sum(factor_expansion_linea) DESC
-        limit 5;
-    """
-    top_5_line_ids = pd.read_sql(q, conn_data)
-    top_5_line_ids = top_5_line_ids.id_linea.to_list()
+    configs = leer_configs_generales()
+    lineas_principales = configs['imprimir_lineas_principales']
+
+    # get top trx id_lines
+    if lineas_principales != 'All':
+        print("Leyendo datos de las lineas con más demanda")
+        q = f"""
+            select id_linea
+            from etapas e
+            where modo = 'autobus'
+            group by id_linea
+            order by sum(factor_expansion_linea) DESC
+            limit {lineas_principales};
+            """
+    else:
+        print("Leyendo datos de las lineas de transporte")
+        q = """
+            select id_linea
+            from etapas e
+            where modo = 'autobus'
+            group by id_linea
+            order by sum(factor_expansion_linea) DESC;
+            """
+    top_line_ids = pd.read_sql(q, conn_data)
+    top_line_ids = top_line_ids.id_linea.to_list()
 
     print("Computar carga por tramo de estas lineas y visualizar")
 
     # Compute and viz route section load by line
     for rango in [[7, 10], [17, 19]]:
         kpi.compute_route_section_load(
-            id_linea=top_5_line_ids, rango_hrs=rango)
+            id_linea=top_line_ids, rango_hrs=rango)
         viz.visualize_route_section_load(
-            id_linea=top_5_line_ids, rango_hrs=rango,
+            id_linea=top_line_ids, rango_hrs=rango,
             save_gdf=True, indicador='prop_etapas', factor=500,
             factor_min=50, )
 
