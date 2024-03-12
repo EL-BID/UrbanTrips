e285bd005270da22af93a4ba08c6e7a9bf411e8d
alephcero <4lephcero@gmail.com>
2024-03-12 11:51:22 -0300

    Add a table for section id line od matrix

--- urbantrips/kpi/section_od_matrix.py
+++ urbantrips/kpi/section_od_matrix.py
@@ -1,7 +1,6 @@
+import warnings
 import pandas as pd
 import geopandas as gpd
-
-from urbantrips.kpi import kpi
 from urbantrips.utils.utils import (
     duracion,
     iniciar_conexion_db,
@@ -12,11 +11,14 @@ from urbantrips.kpi.kpi import (
     create_route_section_ids,
     add_od_lrs_to_legs_from_route
 )
+from urbantrips.kpi.kpi import upload_route_section_points_table
+
+from urbantrips.geo import geo
 
 
 @duracion
 def compute_lines_od_matrix(
-    line_ids=False,
+    line_ids=None,
     hour_range=False,
     n_sections=10,
     section_meters=None,
@@ -48,11 +50,16 @@ def compute_lines_od_matrix(
 
     # check inputs
     check_date_type(day_type)
+    if line_ids is not None:
 
-    if isinstance(line_ids, int):
-        line_ids = [line_ids]
+        if isinstance(line_ids, int):
+            line_ids = [line_ids]
         lines_str = ",".join(map(str, line_ids))
         line_ids_where = f" where id_linea in ({lines_str})"
+
+    else:
+        lines_str = ''
+        line_ids_where = " where id_linea is not NULL"
 
     if n_sections is not None:
         if n_sections > 1000:
@@ -62,19 +69,12 @@ def compute_lines_od_matrix(
     conn_data = iniciar_conexion_db(tipo="data")
     conn_insumos = iniciar_conexion_db(tipo="insumos")
 
-    # Delete old data for those parameters
-    q_delete = delete_old_lines_od_matrix_by_section_data_q(
-        line_ids, hour_range, n_sections, section_meters, day_type
-    )
-    cur = conn_data.cursor()
-    cur.execute(q_delete)
-    conn_data.commit()
-
     # read legs data
-    legs = read_legs_data_by_line_hours_and_day(line_ids, hour_range, day_type)
+    legs = read_legs_data_by_line_hours_and_day(
+        lines_str, hour_range, day_type)
 
     # read routes data
-    q_route_geoms = f"select * from lines_geoms"
+    q_route_geoms = "select * from lines_geoms"
     q_route_geoms = q_route_geoms + line_ids_where
 
     route_geoms = pd.read_sql(q_route_geoms, conn_insumos)
@@ -90,18 +90,26 @@ def compute_lines_od_matrix(
         epsg_m = geo.get_epsg_m()
         # project geoms and get for each geom a n_section
         route_geoms = route_geoms.to_crs(epsg=epsg_m)
-        route_geoms["n_sections"] = (
+        new_n_sections = (
             route_geoms.geometry.length / section_meters).astype(int)
+        route_geoms["n_sections"] = new_n_sections
 
         if (route_geoms.n_sections > 1000).any():
             warnings.warn(
                 "Algunos recorridos tienen mas de 1000 segmentos"
                 "Puede arrojar resultados imprecisos "
             )
-
+        n_sections = new_n_sections
         route_geoms = route_geoms.to_crs(epsg=4326)
-    else:
-        route_geoms["n_sections"] = n_sections
+
+    route_geoms["n_sections"] = n_sections
+
+    upload_route_section_points_table(route_geoms)
+
+    # delete old data
+    yr_mos = legs.yr_mo.unique()
+    delete_old_lines_od_matrix_by_section_data_q(
+        route_geoms, hour_range, day_type, yr_mos)
 
     print("Computing section load per route ...")
 
@@ -136,6 +144,9 @@ def compute_lines_od_matrix(
         line_od_matrix.to_sql(
             "lines_od_matrix_by_section", conn_data, if_exists="append",
             index=False,)
+
+        return line_od_matrix
+
     else:
         print('No existen recorridos o etapas para las líneas')
         print("Cantidad de lineas:", len(line_ids))
@@ -143,7 +154,7 @@ def compute_lines_od_matrix(
         print("Cantidad de etapas", len(legs))
 
 
-def read_legs_data_by_line_hours_and_day(line_id, hour_range, day_type):
+def read_legs_data_by_line_hours_and_day(lines_str, hour_range, day_type):
     """
     Reads legs data by line id, hour range and type of day
 
@@ -176,13 +187,12 @@ def read_legs_data_by_line_hours_and_day(line_id, hour_range, day_type):
     """
 
     # If line and hour, get that subset
-    if line_id:
-        if type(line_id) == int:
-            line_id = [line_id]
-
-        lines_str = ",".join(map(str, line_id))
+    if len(lines_str) > 0:
         line_id_where = f" where id_linea in ({lines_str})"
-        q_main_legs = q_main_legs + line_id_where
+    else:
+        line_id_where = " where id_linea is not NULL"
+
+    q_main_legs = q_main_legs + line_id_where
 
     if hour_range:
         hour_range_where = (
@@ -223,77 +233,49 @@ def read_legs_data_by_line_hours_and_day(line_id, hour_range, day_type):
 
 
 def delete_old_lines_od_matrix_by_section_data_q(
-    id_linea, rango_hrs, n_sections, section_meters, day_type
+    route_geoms, hour_range, day_type, yr_mos
 ):
     """
-    Deletes old data in table 
+    Deletes old data in table lines_od_matrix_by_section
     """
+    conn_data = iniciar_conexion_db(tipo="data")
+
     # hour range filter
-    if rango_hrs:
-        hora_min_filter = f"= {rango_hrs[0]}"
-        hora_max_filter = f"= {rango_hrs[1]}"
+    if hour_range:
+        hora_min_filter = f"= {hour_range[0]}"
+        hora_max_filter = f"= {hour_range[1]}"
     else:
         hora_min_filter = "is NULL"
         hora_max_filter = "is NULL"
 
-    q_delete = f"""
-        delete from lines_od_matrix_by_section
-        where hour_min {hora_min_filter}
-        and hour_max {hora_max_filter}
-        and day_type = '{day_type}'
-        """
+    # create a df with n sections for each line
+    delete_df = route_geoms.reindex(columns=['id_linea', 'n_sections'])
+    for yr_mo in yr_mos:
+        for _, row in delete_df.iterrows():
+            # Delete old data for those parameters
+            print("Borrando datos antiguos de Matriz OD para linea")
+            print(row.id_linea)
+            print(f"{row.n_sections} secciones")
+            print(yr_mo)
+            if hour_range:
+                print(f"y horas desde {hour_range[0]} a {hour_range[1]}")
 
-    # route id filter
-    if id_linea:
+            q_delete = f"""
+                delete from lines_od_matrix_by_section
+                where id_linea = {row.id_linea} 
+                and hour_min {hora_min_filter}
+                and hour_max {hora_max_filter}
+                and day_type = '{day_type}'
+                and n_sections = {row.n_sections}
+                and yr_mo = '{yr_mo}';
+                """
 
-        if type(id_linea) == int:
-            id_linea = [id_linea]
+            cur = conn_data.cursor()
+            cur.execute(q_delete)
+            conn_data.commit()
 
-        lineas_str = ",".join(map(str, id_linea))
-
-        q_delete = q_delete + f" and id_linea in ({lineas_str})"
-
-    if section_meters:
-        q_delete = q_delete + f" and  section_meters = {section_meters}"
-    else:
-        q_delete = (
-            q_delete +
-            f" and n_sections = {n_sections} and section_meters is NULL"
-        )
-    q_delete = q_delete + ";"
-    return q_delete
-
-
-def create_route_section_points_table(row):
-    """
-    Creates a table with route section points from a route geom row 
-    and returns a table with line_id, number of sections and the 
-    xy point for that section
-
-    Parameters
-    ----------
-    row : GeoPandas GeoSeries
-        Row from route geom GeoDataFrame 
-        with geometry, n_sections and line id
-
-    """
-
-    n_sections = row.n_sections
-    route_geom = row.geometry
-    line_id = row.id_linea
-    sections_id = create_route_section_ids(n_sections)
-    points = route_geom.interpolate(sections_id, normalized=True)
-    route_section_points = pd.DataFrame(
-        {
-            'id_linea': [line_id] * len(sections_id),
-            'n_sections': [n_sections] * len(sections_id),
-            'section_id': sections_id,
-            'x': points.map(lambda p: p.x),
-            'y': points.map(lambda p: p.y)
-
-        }
-    )
-    return route_section_points
+    conn_data.close()
+    print("Fin borrado datos previos")
 
 
 def round_section_id_series(series, section_ids):
@@ -311,10 +293,33 @@ def round_section_id_series(series, section_ids):
 
 def compute_line_od_matrix(df, route_geoms, hour_range, day_type):
     """
+    Computes leg od matrix for a line or set of lines using route sections
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+        A legs DataFrame with OD data 
+    route_geoms: geopandas GeoDataFrame
+        A routes GeoDataFrame with routes and number of sections to slice it
+    hour_range : tuple or bool
+        tuple holding hourly range (from,to) and from 0 to 24. Route section
+        load will be computed for legs happening within tat time range.
+        If False it won't filter by hour.
+    day_type: str
+        type of day on which the section load is to be computed. It can take
+        `weekday`, `weekend` or a specific day in format 'YYYY-MM-DD'
+
+    Returns
+    ----------
+    pandas DataFrame
+        A OD matrix with OD by route section id, number of legs for that
+        pair and percentaje of legs for that hour range
+
     """
 
     line_id = df.id_linea.unique()[0]
-    n_sections = route_geoms.n_sections.unique()[0]
+    n_sections = route_geoms.loc[route_geoms.id_linea ==
+                                 line_id, 'n_sections'].item()
 
     print(f"Computing section load id_route {line_id}")
 
