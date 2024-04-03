14c157a94f58e351c6ea3b43001c418b8cd60e9d
alephcero <4lephcero@gmail.com>
2024-04-03 15:24:17 -0300

    Improvement in dash viz for section load

--- urbantrips/dashboard/pages/2_Indicadores de oferta.py
+++ urbantrips/dashboard/pages/2_Indicadores de oferta.py
@@ -21,6 +21,9 @@ import sqlite3
 from shapely import wkt
 from folium import Figure
 from shapely.geometry import LineString
+from urbantrips.viz.viz import (
+    create_squared_polygon)
+from urbantrips.geo import geo
 
 
 def create_linestring(df,
@@ -216,10 +219,6 @@ def crear_mapa_folium(df_agg,
 
 def plot_lineas(lineas, id_linea, nombre_linea, day_type, n_sections, rango):
 
-    gdf = lineas[(lineas.id_linea == id_linea) &
-                 (lineas.day_type == day_type) &
-                 (lineas.n_sections == n_sections)].copy()
-
     gdf_d0 = lineas[(lineas.id_linea == id_linea) &
                     (lineas.day_type == day_type) &
                     (lineas.n_sections == n_sections) &
@@ -229,53 +228,23 @@ def plot_lineas(lineas, id_linea, nombre_linea, day_type, n_sections, rango):
                     (lineas.day_type == day_type) &
                     (lineas.n_sections == n_sections) &
                     (lineas.sentido == 'vuelta')].copy()
+    epsg_m = geo.get_epsg_m()
+    gdf_d0 = gdf_d0.to_crs(epsg=epsg_m)
+    gdf_d1 = gdf_d1.to_crs(epsg=epsg_m)
 
-    indicator = 'prop_etapas'
+    # Arrows
+    flecha_ida_wgs84 = gdf_d0.loc[gdf_d0.section_id ==
+                                  gdf_d0.section_id.min(), 'geometry']
+    flecha_ida_wgs84 = list(flecha_ida_wgs84.item().coords)
+    flecha_ida_inicio_wgs84 = flecha_ida_wgs84[0]
 
-    gdf_d0[indicator] = (gdf_d0['cantidad_etapas'] /
-                         gdf_d0['cantidad_etapas'].sum() * 100).round(2)
-    gdf_d1[indicator] = (gdf_d1['cantidad_etapas'] /
-                         gdf_d1['cantidad_etapas'].sum() * 100).round(2)
+    flecha_vuelta_wgs84 = gdf_d1.loc[gdf_d1.section_id ==
+                                     max(gdf_d1.section_id), 'geometry']
+    flecha_vuelta_wgs84 = list(flecha_vuelta_wgs84.item().coords)
+    flecha_vuelta_fin_wgs84 = flecha_vuelta_wgs84[1]
 
-    # creating plot
-
-    f = plt.figure(tight_layout=True, figsize=(18, 10), dpi=8)
-    gs = f.add_gridspec(nrows=3, ncols=2)
-    ax1 = f.add_subplot(gs[0:2, 0])
-    ax2 = f.add_subplot(gs[0:2, 1])
-    ax3 = f.add_subplot(gs[2, 0])
-    ax4 = f.add_subplot(gs[2, 1])
-
-    font_dicc = {'fontsize': 18,
-                 'fontweight': 'bold'}
-
-    gdf_d0.plot(ax=ax1, color='purple', alpha=.7, linewidth=gdf_d0[indicator])
-    gdf_d1.plot(ax=ax2, color='orange', alpha=.7, linewidth=gdf_d1[indicator])
-
-    ax1.set_axis_off()
-    ax2.set_axis_off()
-
-    ax1.set_title('IDA', fontdict=font_dicc)
-    ax2.set_title('VUELTA', fontdict=font_dicc)
-
-    # Set title and plot axis
-    if indicator == 'cantidad_etapas':
-        title = 'Segmentos del recorrido - Cantidad de etapas'
-        y_axis_lable = 'Cantidad de etapas por sentido'
-    elif indicator == 'prop_etapas':
-        title = 'Segmentos del recorrido - Porcentaje de etapas totales'
-        y_axis_lable = 'Porcentaje del total de etapas'
-    else:
-        raise Exception(
-            "Indicador debe ser 'cantidad_etapas' o 'prop_etapas'")
-
-    if nombre_linea == '':
-        title = f"Id línea {id_linea} - {day_type}\n{rango}"
-    else:
-        title = f"Línea: {nombre_linea.replace('Línea ', '')} - Id línea: {id_linea} - {day_type}\n{rango}"
-
-    f.suptitle(title, fontsize=20)
-
+    # check if route geom is drawn from west to east
+    geom_dir_east = flecha_ida_inicio_wgs84[0] < flecha_vuelta_fin_wgs84[0]
     # Matching bar plot with route direction
     flecha_eo_xy = (0.4, 1.1)
     flecha_eo_text_xy = (0.05, 1.1)
@@ -288,22 +257,6 @@ def plot_lineas(lineas, id_linea, nombre_linea, day_type, n_sections, rango):
     labels_oe = [''] * len(gdf_d0)
     labels_oe[-1] = 'INICIO'
     labels_oe[0] = 'FIN'
-
-    # Arrows
-    flecha_ida_wgs84 = gdf_d0.loc[gdf_d0.section_id ==
-                                  gdf_d0.section_id.min(), 'geometry']
-    flecha_ida_wgs84 = list(flecha_ida_wgs84.item().coords)
-    flecha_ida_inicio_wgs84 = flecha_ida_wgs84[0]
-    flecha_ida_fin_wgs84 = flecha_ida_wgs84[1]
-
-    flecha_vuelta_wgs84 = gdf_d1.loc[gdf_d1.section_id ==
-                                     max(gdf_d1.section_id), 'geometry']
-    flecha_vuelta_wgs84 = list(flecha_vuelta_wgs84.item().coords)
-    flecha_vuelta_inicio_wgs84 = flecha_vuelta_wgs84[0]
-    flecha_vuelta_fin_wgs84 = flecha_vuelta_wgs84[1]
-
-    # check if route geom is drawn from west to east
-    geom_dir_east = flecha_ida_inicio_wgs84[0] < flecha_vuelta_fin_wgs84[0]
 
     # Set arrows in barplots based on reout geom direction
     if geom_dir_east:
@@ -332,12 +285,81 @@ def plot_lineas(lineas, id_linea, nombre_linea, day_type, n_sections, rango):
         gdf_d0 = gdf_d0.sort_values('section_id', ascending=False)
         gdf_d1 = gdf_d1.sort_values('section_id', ascending=False)
 
+    # For direction 0, get the last section of the route geom
+    flecha_ida = gdf_d0.loc[gdf_d0.section_id ==
+                            max(gdf_d0.section_id), 'geometry']
+    flecha_ida = list(flecha_ida.item().coords)
+    flecha_ida_inicio = flecha_ida[1]
+    flecha_ida_fin = flecha_ida[0]
+
+    # For direction 1, get the first section of the route geom
+    flecha_vuelta = gdf_d1.loc[gdf_d1.section_id ==
+                               gdf_d1.section_id.min(), 'geometry']
+    flecha_vuelta = list(flecha_vuelta.item().coords)
+
+    # invert the direction of the arrow
+    flecha_vuelta_inicio = flecha_vuelta[0]
+    flecha_vuelta_fin = flecha_vuelta[1]
+
+    minx, miny, maxx, maxy = gdf_d0.total_bounds
+    box = create_squared_polygon(minx, miny, maxx, maxy, epsg_m)
+
+    # st.dataframe(gdf_d0.drop('geometry', axis=1))
+    # st.dataframe(gdf_d1.drop('geometry', axis=1))
+
+    # creando buffers en base a
+    gdf_d0['geometry'] = gdf_d0.geometry.buffer(gdf_d0.buff_factor)
+    gdf_d1['geometry'] = gdf_d1.geometry.buffer(gdf_d1.buff_factor)
+
+    # creating plot
+
+    f = plt.figure(tight_layout=True, figsize=(18, 10), dpi=8)
+    gs = f.add_gridspec(nrows=3, ncols=2)
+    ax1 = f.add_subplot(gs[0:2, 0])
+    ax2 = f.add_subplot(gs[0:2, 1])
+    ax3 = f.add_subplot(gs[2, 0])
+    ax4 = f.add_subplot(gs[2, 1])
+
+    font_dicc = {'fontsize': 18,
+                 'fontweight': 'bold'}
+    box.plot(ax=ax1, color='#ffffff00')
+    box.plot(ax=ax2, color='#ffffff00')
+
+    try:
+        gdf_d0.plot(ax=ax1, column='legs', cmap='BuPu',
+                    scheme='fisherjenks', k=5, alpha=.6)
+        gdf_d1.plot(ax=ax2, column='legs', cmap='Oranges',
+                    scheme='fisherjenks', k=5, alpha=.6)
+    except ValueError:
+        gdf_d0.plot(ax=ax1, color='purple', alpha=.7,
+                    # linewidth=gdf_d0['buff_factor']
+                    )
+        gdf_d1.plot(ax=ax2, color='orange', alpha=.7,
+                    # linewidth=gdf_d1['buff_factor']
+                    )
+
+    ax1.set_axis_off()
+    ax2.set_axis_off()
+
+    ax1.set_title('IDA', fontdict=font_dicc)
+    ax2.set_title('VUELTA', fontdict=font_dicc)
+
+    title = 'Segmentos del recorrido - Porcentaje de etapas totales'
+    y_axis_lable = 'Porcentaje del total de etapas'
+
+    if nombre_linea == '':
+        title = f"Id línea {id_linea} - {day_type}\n{rango}"
+    else:
+        title = f"Línea: {nombre_linea.replace('Línea ', '')} - Id línea: {id_linea} - {day_type}\n{rango}"
+
+    f.suptitle(title, fontsize=20)
+
     sns.barplot(data=gdf_d0, x="section_id",
-                y=indicator, ax=ax3, color='Purple',
+                y='prop', ax=ax3, color='Purple',
                 order=gdf_d0.section_id.values)
 
     sns.barplot(data=gdf_d1, x="section_id",
-                y=indicator, ax=ax4, color='Orange',
+                y='prop', ax=ax4, color='Orange',
                 order=gdf_d1.section_id.values)
 
     # Axis
@@ -351,7 +373,7 @@ def plot_lineas(lineas, id_linea, nombre_linea, day_type, n_sections, rango):
 
     ax4.set_ylabel('')
     ax4.set_xlabel('')
-    max_y_barplot = max(gdf_d0[indicator].max(), gdf_d1[indicator].max())
+    max_y_barplot = max(gdf_d0['prop'].max(), gdf_d1['prop'].max())
     ax3.set_ylim(0, max_y_barplot)
     ax4.set_ylim(0, max_y_barplot)
 
@@ -361,27 +383,13 @@ def plot_lineas(lineas, id_linea, nombre_linea, day_type, n_sections, rango):
     ax4.spines.right.set_visible(False)
     ax4.spines.top.set_visible(False)
 
-    # For direction 0, get the last section of the route geom
-    flecha_ida = gdf_d0.loc[gdf_d0.section_id ==
-                            max(gdf_d0.section_id), 'geometry']
-    flecha_ida = list(flecha_ida.item().coords)
-    flecha_ida_inicio = flecha_ida[1]
-    flecha_ida_fin = flecha_ida[0]
-
-    # For direction 1, get the first section of the route geom
-    flecha_vuelta = gdf_d1.loc[gdf_d1.section_id ==
-                               gdf_d1.section_id.min(), 'geometry']
-    flecha_vuelta = list(flecha_vuelta.item().coords)
-    # invert the direction of the arrow
-    flecha_vuelta_inicio = flecha_vuelta[0]
-    flecha_vuelta_fin = flecha_vuelta[1]
-
     ax1.annotate('', xy=(flecha_ida_inicio[0],
                          flecha_ida_inicio[1]),
                  xytext=(flecha_ida_fin[0],
                          flecha_ida_fin[1]),
                  arrowprops=dict(facecolor='black',
-                                 edgecolor='black'),
+                                 edgecolor='black',
+                                 shrink=0.2),
                  )
 
     ax2.annotate('', xy=(flecha_vuelta_inicio[0],
@@ -389,7 +397,8 @@ def plot_lineas(lineas, id_linea, nombre_linea, day_type, n_sections, rango):
                  xytext=(flecha_vuelta_fin[0],
                          flecha_vuelta_fin[1]),
                  arrowprops=dict(facecolor='black',
-                                 edgecolor='black'),
+                                 edgecolor='black',
+                                 shrink=0.2),
                  )
 
     ax3.annotate('Sentido', xy=flecha_ida_xy, xytext=flecha_ida_text_xy,
@@ -515,12 +524,12 @@ with st.expander('Cargas por tramos'):
     lineas = levanto_tabla_sql('ocupacion_por_linea_tramo', has_wkt=True)
     nl2 = traigo_nombre_lineas(lineas[['id_linea', 'nombre_linea']])
 
-    lineas.loc[lineas['hora_min'].notna(), 'rango'] = 'de ' + \
-        lineas.loc[lineas['hora_min'].notna(), 'hora_min'].astype(int).astype(str) + ' a ' + \
-        lineas.loc[lineas['hora_max'].notna(), 'hora_max'].astype(
+    lineas.loc[lineas['hour_min'].notna(), 'rango'] = 'de ' + \
+        lineas.loc[lineas['hour_min'].notna(), 'hour_min'].astype(int).astype(str) + ' a ' + \
+        lineas.loc[lineas['hour_max'].notna(), 'hour_max'].astype(
             int).astype(str) + ' hrs'
 
-    lineas.loc[lineas['hora_min'].isna(), 'rango'] = "Todo el dia"
+    lineas.loc[lineas['hour_min'].isna(), 'rango'] = "Todo el dia"
 
     if len(lineas) > 0:
         if len(nl2) > 0:
@@ -570,7 +579,8 @@ with st.expander('Matriz OD por linea'):
 
     if len(matriz) > 0:
         if len(nl3) > 0:
-            nombre_linea_ = col1.selectbox('Línea  ', options=nl3)
+            nombre_linea_ = col1.selectbox(
+                'Línea  ', options=nl3, key='nombre_linea_3')
             id_linea = matriz[matriz.nombre_linea ==
                               nombre_linea_].id_linea.values[0]
         else:
