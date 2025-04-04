0a4a6f8eff888a3953cf846147ddaed7e71c9276
alephcero <felipegonz@gmail.com>
2025-04-04 10:47:41 -0300

    Eliminar run dashboard que no cumple ningun proposito

--- urbantrips/run_all_urbantrips.py
+++ urbantrips/run_all_urbantrips.py
@@ -2,7 +2,6 @@ from urbantrips import initialize_environment
 from urbantrips import process_transactions
 from urbantrips import run_postprocessing
 from urbantrips import create_viz
-from urbantrips import run_dashboard
 
 import warnings
 
@@ -15,7 +14,6 @@ def main():
     process_transactions.main()
     run_postprocessing.main()
     create_viz.main()
-    run_dashboard.main()
     
 if __name__ == "__main__":
     main()
